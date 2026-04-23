273552e9a2527b75cc001074a5b4a13f952ad4f4
Dongnyoung <leedo7182@naver.com>
2026-04-23 19:10:28 +0900

    fix: v3 delete

--- src/main/resources/db/migration/V3__add_member_id_to_product_tables.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE product
-    ADD COLUMN IF NOT EXISTS member_id BIGINT;
-
-ALTER TABLE product_draft
-    ADD COLUMN IF NOT EXISTS member_id BIGINT;
-
-CREATE INDEX IF NOT EXISTS idx_product_member_id ON product (member_id);
-CREATE INDEX IF NOT EXISTS idx_product_draft_member_id ON product_draft (member_id);
